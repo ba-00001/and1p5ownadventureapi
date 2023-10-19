458ba2ac36e0f6aea8083a8b350cf00899ac8798
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-10-19 12:34:43 -0400

    commit

--- app/src/main/java/com/example/and1p5ownadventureapi/MainActivity.kt
+++ app/src/main/java/com/example/and1p5ownadventureapi/MainActivity.kt
@@ -7,32 +7,36 @@ import android.widget.Button
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.RequestParams
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler.JSON
-import com.codepath.asynchttpclient.callback.TextHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
-    var petImageURL = ""
+    var animalImageURL_1 = ""
+    var animalImageURL_2 = ""
+    var animalImageURL_3 = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.getPhotoButton)
-        button.setOnClickListener{
-            //make network call for API
-            getDogImageURL()
-        }
-
+        val imageView = findViewById<ImageView>(R.id.AnimalPhoto1)
+        val imageView2 = findViewById<ImageView>(R.id.AnimalPhoto2)
+        val imageView3 = findViewById<ImageView>(R.id.AnimalPhoto3)
+        getNextImage(button, imageView)
+        getNextImage2(button, imageView2)
+        getNextImage3(button, imageView3)
     }
-    private fun getDogImageURL(){
+
+    /*
+       For Animal #1
+        */
+    private fun getAnimalImageURL_1(){
         val client = AsyncHttpClient()
         client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
                 Log.d("Dog", "response successful")
-                petImageURL = json.jsonObject.getString("message")
+                animalImageURL_1 = json.jsonObject.getString("message")
             }
 
             override fun onFailure(
@@ -41,20 +45,94 @@ class MainActivity : AppCompatActivity() {
                 errorResponse: String,
                 throwable: Throwable?
             ) {
-                Log.d("Dog Error", errorResponse)
-                Log.d("petImageURL", "pet image URL set")
+                Log.e("Error", "Request failed with error: $errorResponse")
+                throwable?.printStackTrace()
+
             }
         }]
     }
 
     private fun getNextImage(button: Button, imageView: ImageView) {
         button.setOnClickListener {
-            getDogImageURL()
+            getAnimalImageURL_1()
 
             Glide.with(this)
-                .load(petImageURL)
+                .load(animalImageURL_1)
                 .fitCenter()
                 .into(imageView)
         }
     }
+
+
+    /*
+      For Animal #2
+       */
+
+private fun getAnimalImageURL_2(){
+    val client = AsyncHttpClient()
+    client["https://api.thecatapi.com/v1/images/search?limit=10", object : JsonHttpResponseHandler() {
+        override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+            Log.d("Cat", "response successful")
+            animalImageURL_2 = json.jsonObject.getString("url")
+        }
+
+        override fun onFailure(
+            statusCode: Int,
+            headers: Headers?,
+            errorResponse: String,
+            throwable: Throwable?
+        ) {
+            Log.e("Error", "Request failed with error: $errorResponse")
+            throwable?.printStackTrace()
+
+        }
+    }]
 }
+
+private fun getNextImage2(button: Button, imageView2: ImageView) {
+    button.setOnClickListener {
+        getAnimalImageURL_2()
+
+        Glide.with(this)
+            .load(animalImageURL_2)
+            .fitCenter()
+            .into(imageView2)
+    }
+}
+/*
+  For Animal #3
+   */
+
+private fun getAnimalImageURL_3(){
+    val client = AsyncHttpClient()
+    client["https://randomfox.ca/floof/?ref=apilist.fun", object : JsonHttpResponseHandler() {
+        override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+            Log.d("fox", "response successful")
+            animalImageURL_3 = json.jsonObject.getString("image")
+        }
+
+        override fun onFailure(
+            statusCode: Int,
+            headers: Headers?,
+            errorResponse: String,
+            throwable: Throwable?
+        ) {
+            Log.e("Error", "Request failed with error: $errorResponse")
+            throwable?.printStackTrace()
+
+        }
+    }]
+}
+
+private fun getNextImage3(button: Button, imageView3: ImageView) {
+    button.setOnClickListener {
+        getAnimalImageURL_3()
+
+        Glide.with(this)
+            .load(animalImageURL_3)
+            .fitCenter()
+            .into(imageView3)
+    }
+}
+}
+
